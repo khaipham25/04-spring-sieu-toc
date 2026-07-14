bdf691228ac2ccfff57104b285216f876722a88d
khaipham25 <ktuan2503@gmail.com>
2026-07-14 17:06:27 +0700

    SpringBoot v4

--- src/main/java/vn/hoidanit/springsieutoc/helper/exception/ResourceAlreadyExistsException.java
+++ src/main/java/vn/hoidanit/springsieutoc/helper/exception/ResourceAlreadyExistsException.java
@@ -1,6 +1,9 @@
 package vn.hoidanit.springsieutoc.helper.exception;
 
+import java.io.Serial;
+
 public class ResourceAlreadyExistsException extends RuntimeException{
+    @Serial
     private static final long serialVersionUID = 1L;
 
     public ResourceAlreadyExistsException(String message) {
--- src/main/java/vn/hoidanit/springsieutoc/helper/exception/ResourceNotFoundException.java
+++ src/main/java/vn/hoidanit/springsieutoc/helper/exception/ResourceNotFoundException.java
@@ -1,6 +1,9 @@
 package vn.hoidanit.springsieutoc.helper.exception;
 
+import java.io.Serial;
+
 public class ResourceNotFoundException extends RuntimeException {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     public ResourceNotFoundException(String message) {
